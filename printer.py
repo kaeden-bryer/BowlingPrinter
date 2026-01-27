98d614367644aba2609cfe7c8bc266ea18da37b5
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2026-01-27 18:53:53 -0500

    add end tokens

--- printer.py
+++ printer.py
@@ -6,6 +6,15 @@ with open("input.json", "r") as file:
 lanes = data["Lanes"]
 free_agents = data["Free_Agents"]
 
+end_tokens = {
+    6: "✅",
+    5: "🟢",
+    4: "🟠",
+    3: "🟠",
+    2: "⚠️",
+    1: "⚠️"
+}
+
 def generate():
     with open("output.txt", "w") as output:
         counter = 1
@@ -16,10 +25,7 @@ def print_lanes(counter, output):
     output.write("Lanes:\n")
     for lane in enumerate(lanes):
         slots_filled = len(lane[1])
-        if slots_filled == 6:
-            end_token = "✅"
-        else:
-            end_token = "🟠"
+        end_token = end_tokens[slots_filled]
         output.write(f"Lane {lane[0] + 1} –– " + lane[1][0] + f" ({slots_filled}/6 filled) {end_token}\n")
         for person in lane[1]:
             output.write(f"{counter}. {person}\n")
@@ -33,5 +39,7 @@ def print_free_agents(counter, output):
         output.write(f"{counter}. {person}\n")
         counter += 1
     return counter
+
+
 
 generate()
