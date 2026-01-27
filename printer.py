2ba4ee29e99a06cea989c8168c67709664651aa9
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2026-01-27 17:50:42 -0500

    Update slightly

--- printer.py
+++ printer.py
@@ -16,7 +16,7 @@ def generate():
 def print_lanes(counter, output):
     output.write("Lanes:\n")
     for lane in enumerate(lanes):
-        output.write(f"Lane {lane[0] + 1}\n")
+        output.write(f"Lane {lane[0] + 1} –– " + lane[1][0] + "\n")
         for person in lane[1]:
             output.write(f"{counter}. {person}\n")
             counter += 1
